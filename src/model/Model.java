16b9244154655ba6fe1a109ebf48a9714319605d
0g1 <ogicar000@gmail.com>
2017-10-28 20:29:59 +0200

    Add bullet logic

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,15 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 import main.Constants;
+import view.Updatable;
 
 public class Model {
 	
 	private Player topPlayer;
 	private Player bottomPlayer;
+	private ArrayList<Updatable> updatables;
 	
 	public Model() {
 		this.topPlayer = new Player(Constants.TOP_PLAYER_START_POS);
 		this.bottomPlayer = new Player(Constants.BOTTOM_PLAYER_START_POS);
+		this.updatables = new ArrayList<Updatable>();
+		this.updatables.add(topPlayer);
+		this.updatables.add(bottomPlayer);
 	}
 	
 	public Player getTopPlayer() {
@@ -21,7 +28,12 @@ public class Model {
 	}
 	
 	public void update(double dt) {
-		topPlayer.update(dt);
-		bottomPlayer.update(dt);
+		for (Updatable updatable : updatables) {
+			updatable.update(dt);
+		}
+	}
+	
+	public void addUpdatable(Updatable updatable) {
+		this.updatables.add(updatable);
 	}
 }
