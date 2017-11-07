9786b543b78ab0055f9feee46b6bf16e64aa181b
David Davidović <david@davidovic.io>
2017-11-07 20:17:30 +0100

    Really change additive blending implementation

--- src/util/ImageCache.java
+++ src/util/ImageCache.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
@@ -21,11 +22,19 @@ public class ImageCache {
 		return instance;
 	}
 	
+	private static BufferedImage convertToType(BufferedImage image, int type) {
+	    BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), type);
+	    Graphics2D graphics = newImage.createGraphics();
+	    graphics.drawImage(image, 0, 0, null);
+	    graphics.dispose();
+	    return newImage;
+	}
+	
 	public BufferedImage get(String filename) {
 		if (cache.containsKey(filename)) {
 			return cache.get(filename);
 		} else {
-			BufferedImage image = Util.loadImage(filename);
+			BufferedImage image = convertToType(Util.loadImage(filename), BufferedImage.TYPE_INT_ARGB);
 			cache.put(filename, image);
 			
 			return image;
--- src/view/gfx/AdditiveCompositeContext.java
+++ src/view/gfx/AdditiveCompositeContext.java
@@ -10,34 +10,57 @@ public class AdditiveCompositeContext implements CompositeContext {
 		this.q = q;
 	};
 
-	private static int addChannel(int a, int b) {
-		return Math.min(a + b, 255);
+	private static void addChannels(int[] a, int[] b, int[] dst) {
+		for (int i = 0; i < 3; i++) {
+			dst[i] = Math.min(a[i] + b[i], 255);
+		}
 	}
 	
-	// Don't call with weird image formats, Bad Things(tm) will happen.
 	public void compose(Raster src, Raster dstIn, WritableRaster dstOut) {
-		int w1 = src.getSampleModel().getWidth();
-		int w2 = dstIn.getWidth();
-		int wOut = dstOut.getWidth();
-		int chan1 = src.getNumBands();
-		int chan2 = dstIn.getNumBands();
-		int chanOut = dstOut.getNumBands();
+		if (src.getSampleModel().getDataType() != DataBuffer.TYPE_INT
+				|| dstIn.getSampleModel().getDataType() != DataBuffer.TYPE_INT
+				|| dstOut.getSampleModel().getDataType() != DataBuffer.TYPE_INT) {
+			throw new IllegalStateException("Source and destination must store pixels as INT.");
+		}
 
-		int minCh = Math.min(Math.min(chan1, chan2), chanOut);
-		
-		for (int y = 0; y < dstIn.getHeight(); y++) {
-			for (int x = 0; x < wOut; x++) {
-		        float[] pxSrc = null;
-		        pxSrc = src.getPixel(x, y, pxSrc);
-		        float[] pxDst = null;
-		        pxDst = dstIn.getPixel(x, y, pxDst);
-		        
-		        for(int i = 0; i < 3 && i < minCh; i++) {
-		          pxDst[i] = (float)Math.min(255, (pxSrc[i] * q) + (pxDst[i]));
-		          dstOut.setPixel(x, y, pxDst);
-		        }
-						
+		int width = Math.min(src.getWidth(), dstIn.getWidth());
+		int height = Math.min(src.getHeight(), dstIn.getHeight());
+
+		float alpha = (float)q;
+
+		int[] srcPixel = new int[4];
+		int[] dstPixel = new int[4];
+		int[] srcPixels = new int[width];
+		int[] dstPixels = new int[width];
+
+		int[] result = new int[4];
+		for (int y = 0; y < height; y++) {
+			src.getDataElements(0, y, width, 1, srcPixels);
+			dstIn.getDataElements(0, y, width, 1, dstPixels);
+			for (int x = 0; x < width; x++) {
+				// pixels are stored as INT_ARGB
+				// our arrays are [R, G, B, A]
+				int pixel = srcPixels[x];
+				srcPixel[0] = (pixel >> 16) & 0xFF;
+				srcPixel[1] = (pixel >> 8) & 0xFF;
+				srcPixel[2] = (pixel) & 0xFF;
+				srcPixel[3] = (pixel >> 24) & 0xFF;
+
+				pixel = dstPixels[x];
+				dstPixel[0] = (pixel >> 16) & 0xFF;
+				dstPixel[1] = (pixel >> 8) & 0xFF;
+				dstPixel[2] = (pixel) & 0xFF;
+				dstPixel[3] = (pixel >> 24) & 0xFF;
+
+				addChannels(srcPixel, dstPixel, result);
+
+				// mixes the result with the opacity
+				dstPixels[x] = ((int) (dstPixel[3] + (result[3] - dstPixel[3]) * alpha) & 0xFF) << 24
+						| ((int) (dstPixel[0] + (result[0] - dstPixel[0]) * alpha) & 0xFF) << 16
+						| ((int) (dstPixel[1] + (result[1] - dstPixel[1]) * alpha) & 0xFF) << 8
+						| (int) (dstPixel[2] + (result[2] - dstPixel[2]) * alpha) & 0xFF;
 			}
+			dstOut.setDataElements(0, y, width, 1, dstPixels);
 		}
 	}
 
