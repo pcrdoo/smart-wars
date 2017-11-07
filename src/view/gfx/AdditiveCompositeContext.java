2433b9ac0c8802f12f126c4ce23f0bfe9550ff00
David Davidović <david@davidovic.io>
2017-11-07 18:55:44 +0100

    Fix additive compositor on Mac

--- src/view/gfx/AdditiveCompositeContext.java
+++ src/view/gfx/AdditiveCompositeContext.java
@@ -25,25 +25,8 @@ public class AdditiveCompositeContext implements CompositeContext {
 
 		int minCh = Math.min(Math.min(chan1, chan2), chanOut);
 		
-		byte[] srcBuf = ((DataBufferByte)src.getDataBuffer()).getData();
-		int[] dstBuf = ((DataBufferInt)dstIn.getDataBuffer()).getData();
-		int[] outBuf = ((DataBufferInt)dstOut.getDataBuffer()).getData();
-
 		for (int y = 0; y < dstIn.getHeight(); y++) {
 			for (int x = 0; x < wOut; x++) {
-				/*int ra = srcBuf[(x + y * w1) * 3 + 2] & 0xFF;
-				int ga = srcBuf[(x + y * w1) * 3 + 1] & 0xFF;
-				int ba = srcBuf[(x + y * w1) * 3 + 0] & 0xFF;
-				
-				int b = dstBuf[x + y * w2];
-				int rb = (b >> 16) & 0xFF;
-				int gb = (b >> 8) & 0xFF;
-				int bb = (b >> 0) & 0xFF;
-				
-				outBuf[x + y * wOut] = 0xFF000000 + 
-						(addChannel((int)(ra * q), rb) << 16) +
-						(addChannel((int)(ga * q), gb) << 8) +
-						(addChannel((int)(ba * q), bb) << 0);*/
 		        float[] pxSrc = null;
 		        pxSrc = src.getPixel(x, y, pxSrc);
 		        float[] pxDst = null;
