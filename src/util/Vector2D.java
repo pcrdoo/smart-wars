292e3bf797576ed445271e085ade28e94cba6d77
Rand0mUsername <njov996@gmail.com>
2017-11-13 13:51:22 +0100

    Refactor Vector2D

--- src/util/Vector2D.java
+++ src/util/Vector2D.java
@@ -1,123 +1,97 @@
 package util;
 
-import java.lang.Math;
 import java.nio.ByteBuffer;
 
 public class Vector2D {
 
-	protected double dX;
-	protected double dY;
-
-	// Constructor methods ....
+	protected double x;
+	protected double y;
 
 	public Vector2D() {
-		dX = dY = 0.0;
+		x = y = 0.0;
 	}
 
-	public Vector2D(double dX, double dY) {
-		this.dX = dX;
-		this.dY = dY;
+	public Vector2D(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 
-	// Convert vector to a string ...
-
+	@Override
 	public String toString() {
-		return "Vector2D(" + dX + ", " + dY + ")";
+		return "Vector2D(" + x + ", " + y + ")";
 	}
-
-	// Compute magnitude of vector ....
 
 	public double length() {
-		return Math.sqrt(dX * dX + dY * dY);
+		return Math.sqrt(x * x + y * y);
 	}
 
-	// Sum of two vectors ....
-
-	public Vector2D add(Vector2D v1) {
-		Vector2D v2 = new Vector2D(this.dX + v1.dX, this.dY + v1.dY);
-		return v2;
+	public Vector2D add(Vector2D other) {
+		Vector2D ret = new Vector2D(x + other.x, y + other.y);
+		return ret;
 	}
 
-	// Subtract vector v1 from v .....
-
-	public Vector2D sub(Vector2D v1) {
-		Vector2D v2 = new Vector2D(this.dX - v1.dX, this.dY - v1.dY);
-		return v2;
+	public Vector2D sub(Vector2D other) {
+		Vector2D ret = new Vector2D(x - other.x, y - other.y);
+		return ret;
 	}
-
-	// Scale vector by a constant ...
 
 	public Vector2D scale(double scaleFactor) {
-		Vector2D v2 = new Vector2D(this.dX * scaleFactor, this.dY * scaleFactor);
-		return v2;
+		Vector2D ret = new Vector2D(x * scaleFactor, y * scaleFactor);
+		return ret;
 	}
-
-	// Normalize a vectors length....
 
 	public Vector2D normalize() {
-		Vector2D v2 = new Vector2D();
+		Vector2D ret = new Vector2D();
 		double len = length();
 		if (len != 0) {
-			v2 = this.scale(1 / len);
+			ret = scale(1 / len);
 		}
-		return v2;
+		return ret;
 	}
 
-	// Dot and cross product of two vectors .....
-
-	public double dotProduct(Vector2D v1) {
-		return this.dX * v1.dX + this.dY * v1.dY;
+	public double dotProduct(Vector2D other) {
+		return x * other.x + y * other.y;
 	}
 
-	public double crossProductIntensity(Vector2D v1) {
-		return this.dX * v1.dY - this.dY * v1.dX;
+	public double crossProductIntensity(Vector2D other) {
+		return x * other.y - y * other.x;
 	}
 
-	// Clamp coordinates...
-
-	public void clampX(double mindX, double maxdX) {
-		if (this.dX < mindX) {
-			setX(mindX);
-		}
-		if (this.dX > maxdX) {
-			setX(maxdX);
-		}
+	public void clampX(double minx, double maxx) {
+		x = Math.max(x, minx);
+		x = Math.min(x, maxx);
 	}
 
-	public void clampY(double mindY, double maxdY) {
-		if (this.dY < mindY) {
-			setY(mindY);
-		}
-		if (this.dY > maxdY) {
-			setY(maxdY);
-		}
+	public void clampY(double miny, double maxy) {
+		y = Math.max(y, miny);
+		y = Math.min(y, maxy);
 	}
 
-	public void setX(double dX) {
-		this.dX = dX;
+	public void setX(double x) {
+		this.x = x;
 	}
-	
-	public void setY(double dY) {
-		this.dY = dY;
+
+	public void setY(double y) {
+		this.y = y;
 	}
-	
+
 	public double getX() {
-		return dX;
+		return x;
 	}
 
 	public double getY() {
-		return dY;
+		return y;
 	}
-	
+
 	public void serializeTo(ByteBuffer buffer) {
-		buffer.putFloat((float)dX);
-		buffer.putFloat((float)dY);
+		buffer.putFloat((float) x);
+		buffer.putFloat((float) y);
 	}
-	
-	public static Vector2D deserializeFrom(ByteBuffer buffer)  {
+
+	public static Vector2D deserializeFrom(ByteBuffer buffer) {
 		return new Vector2D(buffer.getFloat(), buffer.getFloat());
 	}
-	
+
 	public static int getSerializedSize() {
 		return 2 * 4;
 	}
